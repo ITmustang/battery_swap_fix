615c9a35690bf45b327056828472e8b1e23e612a
ITMustang <ITmustang@users.noreply.github.com>
2025-01-10 15:54:43 -0500

    Update battery_check.sh

--- battery_check.sh
+++ battery_check.sh
@@ -5,14 +5,32 @@
 info() {
   bat=$1
   name=$(basename "$1")
-  status=$(cat $bat/status)
-  capacity=$(cat $bat/capacity)
-  start=$(cat $bat/charge_start_threshold)
-  stop=$(cat $bat/charge_stop_threshold)
-  printf "%s: %-12.12s, capacity: %-10s, start: %-10s, stop: %s\n" \
-	"$name" "$status" "$capacity" "$start" "$stop"
+  status=$(cat "$bat/status")
+  
+  # Check for charge or energy files
+  if [ -f "$bat/charge_now" ] && [ -f "$bat/charge_full" ]; then
+    charge_now=$(cat "$bat/charge_now")
+    charge_full=$(cat "$bat/charge_full")
+    capacity=$(awk "BEGIN {printf \"%.0f\", ($charge_now / $charge_full) * 100}")
+  else
+    capacity="N/A"
+  fi
+
+  # Check for charge threshold files
+  if [ -f "$bat/charge_start_threshold" ] && [ -f "$bat/charge_stop_threshold" ]; then
+    start=$(cat "$bat/charge_start_threshold")
+    stop=$(cat "$bat/charge_stop_threshold")
+  else
+    start="N/A"
+    stop="N/A"
+  fi
+
+  printf "%s: %-12.12s, capacity: %-10s%%, start: %-10s, stop: %s\n" \
+        "$name" "$status" "$capacity" "$start" "$stop"
 }
 
-for bat in $(ls -d /sys/class/power_supply/BAT*); do
-  info "$bat"
+for bat in /sys/class/power_supply/BAT*; do
+  if [ -d "$bat" ]; then
+    info "$bat"
+  fi
 done
